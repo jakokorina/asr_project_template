052981ceffab94484e64ddeb9349eb49001a6726
jakokorina <jakokorina@mail.ru>
2022-10-16 17:51:37 +0300

    -

--- test.py
+++ test.py
@@ -63,11 +63,11 @@ def main(config, out_file):
                 argmax = argmax[: int(batch["log_probs_length"][i])]
                 results.append(
                     {
-                        "ground_trurh": batch["text"][i],
+                        "ground_truth": batch["text"][i],
                         "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy()),
-                        "pred_text_beam_search": text_encoder.ctc_beam_search(
-                            batch["probs"][i], batch["log_probs_length"][i], beam_size=100
-                        )[:10],
+                        "pred_text_beam_search": text_encoder.beam_search(
+                            batch["log_probs"][i][:batch["log_probs_length"][i]], beam_size=100
+                        ),
                     }
                 )
     with Path(out_file).open("w") as f:
